4288ff7e79fccc6488b1c5f4ea3d44f1dfd5e8a6
marko <marko@fikret>
2012-12-03 16:33:58 +0100

    Implemented the form for registering subscribers.

--- kcclass-newsletter-sender/src/main/java/si/kcclass/newslettersender/web/SubscriberController.java
+++ kcclass-newsletter-sender/src/main/java/si/kcclass/newslettersender/web/SubscriberController.java
@@ -1,8 +1,15 @@
 package si.kcclass.newslettersender.web;
 
+import javax.validation.Valid;
+
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import si.kcclass.newslettersender.domain.Advertiser;
 import si.kcclass.newslettersender.domain.Subscriber;
 
 @RequestMapping("/subscribers")
@@ -10,7 +17,20 @@ import si.kcclass.newslettersender.domain.Subscriber;
 @RooWebScaffold(path = "subscribers", formBackingObject = Subscriber.class)
 public class SubscriberController {
 	
-	public void registerSubscriber() {
-		
+    @RequestMapping(value = "register-subscriber/{advertiser_id}", method=RequestMethod.GET, produces = "text/html")
+    public String registerSubscriberForm(
+    		@PathVariable("advertiser_id") Long advertiserId, Model uiModel) {
+    	Advertiser advertiser = Advertiser.findAdvertiser(advertiserId);
+    	Subscriber subscriber = new Subscriber();
+    	subscriber.setAdvertiser(advertiser);
+    	uiModel.addAttribute("subscriber", subscriber);
+    	uiModel.addAttribute("advertiserId", advertiserId);
+        return "subscribers/registerSubscriber";
+    }
+
+    @RequestMapping(value = "register-subscriber", method = RequestMethod.POST, produces = "text/html")
+	public String registerSubscriber(@Valid Subscriber subscriber) {
+    	return "redirect:/subscribers";
 	}
+	
 }
